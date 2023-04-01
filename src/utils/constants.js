91cc49e8185958f7db3337338ac1bfc191b0efa0
Sankalp Sharma <sharmasankalp@outlook.com>
2023-04-01 08:47:32 +0530

    added https instead of http

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const SUGGESTIONS_CACHE_COUNT = 10
 export const WEBSITE_URL = "https://youtube-frontend-sankalp.vercel.app/"
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + YOUR_API_KEY
-export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const YOUTUBE_COMMENTS_API = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=[videoID]&key=" + YOUR_API_KEY
 export const YOUTUBE_VIDEO_INFO_API = "https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=[videoID]&key=" + YOUR_API_KEY
